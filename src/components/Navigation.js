fb0070d6c0749751fa34d9a1194d4097a37c7115
Emiliano Botello <ebotello@codeservices.com.mx>
2021-11-09 15:33:16 -0800

    URLS

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,20 +6,20 @@ export class Navigation extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Notes</Link>
+                    <Link className="navbar-brand" to="/practica4-7frontend/">Notes</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link" to="/">Notes</Link>
+                                <Link className="nav-link" to="/practica4-7frontend/">Notes</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/create">Create Note</Link>
+                                <Link className="nav-link" to="/practica4-7frontend/create">Create Note</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/user">Create User</Link>
+                                <Link className="nav-link" to="/practica4-7frontend/user">Create User</Link>
                             </li>
                         </ul>
                     </div>
